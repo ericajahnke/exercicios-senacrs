aab9bef89c9cfec1b7e96bd985df4e1ba2b3d048
érica <141285221+ericajahnke@users.noreply.github.com>
2024-09-16 14:13:47 -0300

    Update ex2.js

--- ex. aula 4-2/ex2.js
+++ ex. aula 4-2/ex2.js
@@ -1,22 +1,22 @@
 // Imprimir um triângulo de asteriscos com 4 linhas.
 prompt = require("prompt-sync")();
-let linhas;
+let linhas
 
 while (true) {
-  linhas = Number(prompt("Digite o número de linhas do triângulo: "));
+  linhas = Number(prompt("Digite o número de linhas do triângulo: "))
   if (linhas <= 0) {
-    console.log("Entrada inválida. Por favor, insira um número positivo.");
-    continue;
+    console.log("Entrada inválida. Por favor, insira um número positivo.")
+    continue
   }
   for (let i = 1; i <= linhas; i++) {
     let linha = "";
     for (let j = 1; j <= linhas - i; j++) {
-      linha += " ";
+      linha += " "
     }
     for (let j = 1; j <= i; j++) {
-      linha += "* ";
+      linha += "* "
     }
-    console.log(linha);
+    console.log(linha)
   }
-  break;
+  break
 }
