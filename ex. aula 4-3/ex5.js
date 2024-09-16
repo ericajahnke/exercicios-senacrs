55895439b6c6ef78d84c0d5340e9778395d495f1
érica <141285221+ericajahnke@users.noreply.github.com>
2024-09-16 14:15:32 -0300

    Update ex5.js

--- ex. aula 4-3/ex5.js
+++ ex. aula 4-3/ex5.js
@@ -1,22 +1,14 @@
 // Escreva um algoritmo para ler 2 valores e imprimir o resultado da divisão do primeiro pelo segundo. Se o segundo valor informado for ZERO, deve ser lido um novo valor.
 // Acrescente uma mensagem de ‘VALOR INVÁLIDO’ no exercício 5, caso o segundo valor informado seja ZERO.
 prompt = require("prompt-sync")()
-const lerValor = (mensagem, resultado) => {
-let valor
+let valor1 = Number(prompt("Digite o primeiro valor: "))
+let valor2 = Number(prompt("Digite o segundo valor: "))
 
- while (true) {
- valor = Number(prompt(mensagem))
-  if (valor !== 0) {
-  resultado.value = valor
-  break
- }
- console.log("VALOR INVÁLIDO. O valor não pode ser ZERO.");
- }
+while (valor2 === 0) {
+  console.log("Valor inválido")
+  valor2 = Number(prompt("Digite um novo valor para o segundo número: "))
 }
 
-let valor1 = { value: 0 };
-let valor2 = { value: 0 };
-lerValor("Digite o primeiro valor: ", valor1);
-lerValor("Digite o segundo valor (diferente de ZERO): ", valor2);
-let resultado = valor1.value / valor2.value;
-console.log(`O resultado da divisão é: ${resultado.toFixed(1)}`);
+let resultado = valor1 / valor2
+
+console.log(`O resultado da divisão é: ${resultado}`)
