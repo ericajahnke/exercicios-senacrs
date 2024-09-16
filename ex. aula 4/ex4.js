aa33f40deec2f14db68866215d5a46633b31728d
érica <141285221+ericajahnke@users.noreply.github.com>
2024-09-16 13:54:45 -0300

    Update ex4.js

--- ex. aula 4/ex4.js
+++ ex. aula 4/ex4.js
@@ -1,7 +1,8 @@
 // Imprimir a sequência de Fibonacci até o 10º termo.
 prompt = require("prompt-sync")()
 let n = 10
-let a = 0, b = 1
+let a = 0
+let b = 1
 
 console.log("Sequência de Fibonacci até o 10º termo:")
 for(let i = 1; i <= n; i++) {
