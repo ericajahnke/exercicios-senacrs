d3af78c004c836ac426ec31dd6f82e57dab17c00
érica <141285221+ericajahnke@users.noreply.github.com>
2024-09-17 17:00:46 -0300

    Exercícios.

--- ex. aula 5/ex1.js
+++ ex. aula 5/ex1.js
@@ -1,4 +1,4 @@
 // Crie um vetor que contenha os nomes de cinco frutas. Exiba o terceiro elemento do vetor no console.
 prompt = require("prompt-sync")()
 const frutas = ["morango", "mamão", "banana", "tangerina", "uva"]
-console.log(`A terceira fruta do vetor é: ${frutas[2]}!`)
+console.log(`A terceira fruta do vetor é: ${frutas[2]}!`) 
