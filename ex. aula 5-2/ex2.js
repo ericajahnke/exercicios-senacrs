271d68f979376e7da6bc11deafef740a540b551c
érica <141285221+ericajahnke@users.noreply.github.com>
2024-09-18 16:55:26 -0300

    Exercícios.

--- ex. aula 5-2/ex2.js
+++ ex. aula 5-2/ex2.js
@@ -1,1 +1,9 @@
 // Crie um vetor de cinco números aleatórios. Ordene o vetor em ordem crescente e exiba o resultado.
+const numeros = [5, 2, 17, 23, 12]
+const ordem = numeros.sort((a, b) => {
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+})
+
+console.log(ordem)
