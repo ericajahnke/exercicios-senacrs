b078983cf6fb2b21e9ca0793dda496ba1615ea3f
érica <141285221+ericajahnke@users.noreply.github.com>
2024-09-18 16:54:20 -0300

    Exercícios.

--- ex. aula 5-2/ex1.js
+++ ex. aula 5-2/ex1.js
@@ -1,1 +1,9 @@
 // Crie um vetor de três números inteiros. Multiplique cada elemento por 2 e exiba o vetor resultante.
+const numeros = [5, 2, 17, 23, 12]
+const ordem = numeros.sort((a, b) => {
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+})
+
+console.log(ordem)
